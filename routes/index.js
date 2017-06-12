659d6255f497ba4f12940a3746aac6abdfd08263
Dashy10 <dashiell.lumas@gmail.com>
2017-06-12 10:23:02 -0400

    new matches route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/matches', function(req, res, next) {
+  res.render('matches', { title: 'Express' });
+});
 module.exports = router;

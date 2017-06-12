9c594ff9a075e93acb546051ee75528b07c33219
Dashy10 <dashiell.lumas@gmail.com>
2017-06-12 11:35:40 -0400

    added login component

--- public/javascripts/style.js
+++ public/javascripts/style.js
@@ -1,11 +1,24 @@
+// testing testing....
 console.log('Is connected');
 
+
+// modal functions
+function showLogin() {
+  document.getElementById('login-form').style.display = "block";
+};
+
+function hideLogin() {
+  document.getElementById('login-form').style.display = "none";
+};
+
+// jquery
 $(function() {
+  // hero cover to sticky nav
   $(window).scroll(function() {
     var winTop = $(window).scrollTop();
     if (winTop >= 30) {
       $("body").addClass("sticky-shrinknav-wrapper");
-    } else{
+    } else {
       $("body").removeClass("sticky-shrinknav-wrapper");
     }
   });

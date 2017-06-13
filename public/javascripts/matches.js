b063fb201c98fab1dffc5f2704ca987fe2653972
Dashy10 <dashiell.lumas@gmail.com>
2017-06-13 13:15:59 -0400

    basic functionality for how it works complete

--- public/javascripts/matches.js
+++ public/javascripts/matches.js
@@ -1,5 +1,93 @@
 console.log('matches.js is here')
 
+function showFrench() {
+  $('.english').css("display", "block");
+  $('.spanish').css("display", "block");
+  $('.russian').css("display", "block");
+  $('.icelandic').css("display", "block");
+  $('.czech').css("display", "block");
+  $('.french').css("display", "block");
+
+  $('.english').css("display", "none");
+  $('.spanish').css("display", "none");
+  $('.russian').css("display", "none");
+  $('.icelandic').css("display", "none");
+  $('.czech').css("display", "none");
+};
+function showEnglish() {
+  $('.english').css("display", "block");
+  $('.spanish').css("display", "block");
+  $('.russian').css("display", "block");
+  $('.icelandic').css("display", "block");
+  $('.czech').css("display", "block");
+  $('.french').css("display", "block");
+
+  $('.french').css("display", "none");
+  $('.spanish').css("display", "none");
+  $('.russian').css("display", "none");
+  $('.icelandic').css("display", "none");
+  $('.czech').css("display", "none");
+};
+function showSpanish() {
+  $('.english').css("display", "block");
+  $('.spanish').css("display", "block");
+  $('.russian').css("display", "block");
+  $('.icelandic').css("display", "block");
+  $('.czech').css("display", "block");
+  $('.french').css("display", "block");
+
+  $('.english').css("display", "none");
+  $('.french').css("display", "none");
+  $('.russian').css("display", "none");
+  $('.icelandic').css("display", "none");
+  $('.czech').css("display", "none");
+};
+
+function showRussian() {
+  $('.english').css("display", "block");
+  $('.spanish').css("display", "block");
+  $('.russian').css("display", "block");
+  $('.icelandic').css("display", "block");
+  $('.czech').css("display", "block");
+  $('.french').css("display", "block");
+
+  $('.english').css("display", "none");
+  $('.spanish').css("display", "none");
+  $('.french').css("display", "none");
+  $('.icelandic').css("display", "none");
+  $('.czech').css("display", "none");
+};
+
+function showIcelandic() {
+  $('.english').css("display", "block");
+  $('.spanish').css("display", "block");
+  $('.russian').css("display", "block");
+  $('.icelandic').css("display", "block");
+  $('.czech').css("display", "block");
+  $('.french').css("display", "block");
+
+  $('.english').css("display", "none");
+  $('.spanish').css("display", "none");
+  $('.russian').css("display", "none");
+  $('.french').css("display", "none");
+  $('.czech').css("display", "none");
+};
+
+function showCzech() {
+  $('.english').css("display", "block");
+  $('.spanish').css("display", "block");
+  $('.russian').css("display", "block");
+  $('.icelandic').css("display", "block");
+  $('.czech').css("display", "block");
+  $('.french').css("display", "block");
+
+  $('.english').css("display", "none");
+  $('.spanish').css("display", "none");
+  $('.russian').css("display", "none");
+  $('.icelandic').css("display", "none");
+  $('.french').css("display", "none");
+};
+
 // function getAllUsers() {
 //   let allusers = 'http://localhost:3000/api/users'
 //   axios.get(allusers).then(function(res) {

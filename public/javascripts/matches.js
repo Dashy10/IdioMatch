30cec5793db65f40d4ac2531dcffc1560fb065b1
Dashy10 <dashiell.lumas@gmail.com>
2017-06-13 11:16:25 -0400

    containers organized by class

--- public/javascripts/matches.js
+++ public/javascripts/matches.js
@@ -1,20 +1,23 @@
 console.log('matches.js is here')
 
-function getAllUsers() {
-  let allusers = 'http://localhost:3000/api/users'
-  axios.get(allusers).then(function(res) {
-    let allData = res.data.data;
-    allData.forEach(function(arrayItem) {
-      console.log(arrayItem.username);
-      console.log(arrayItem.main_language);
-      console.log(arrayItem.learning_language);
-      console.log(arrayItem.email);
-      console.log(arrayItem.skype);
-      console.log(arrayItem.whatsapp);
-      document.body.innerHtml = arrayItem.username;
-      // var user_container = document.createElement("div")
-
-    });
-
-  });
-};
+// function getAllUsers() {
+//   let allusers = 'http://localhost:3000/api/users'
+//   axios.get(allusers).then(function(res) {
+//     let allData = res.data.data;
+//     for(let i = 0; i < allData.length; i++){
+//
+//     }
+//
+//
+//     allData.forEach(function(arrayItem) {
+//       console.log(arrayItem.username);
+//       console.log(arrayItem.main_language);
+//       console.log(arrayItem.learning_language);
+//       console.log(arrayItem.email);
+//       console.log(arrayItem.skype);
+//       console.log(arrayItem.whatsapp);
+//
+//     });
+//
+//   });
+// };
